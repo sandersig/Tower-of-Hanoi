6322feadb6ec295a393971d3c0714b30bb4c8557
Sander Sigmundstad <sanderls@icloud.com>
2021-10-28 11:25:17 +0200

    The game now works as intended. Added win-statement."

--- Oblig2.hs
+++ Oblig2.hs
@@ -1,25 +1,30 @@
-start moves height tower1 tower2 tower3 = do 
-          --print tower1
-          --print tower2
-          --    print tower3
-          c <- getLine
-          let com = words c
-          if(head com /= "q") then
-              if(head com == "b") then do -- TODO: resette listene ved opprettelse av nytt brett
-                  clr
-                  let moves = 0 
-                  let height = read (head (tail com)) :: Int
-                  if(height <= 1 || height > 12) then do
-                      print "Unvalid nr of rings. The nr of rings needs to be between 2 and 12."
-                      start 0 1 tower1 tower2 tower3
-                  else do
+
+
+start moves height tower1 tower2 tower3 = 
+    if(length tower3 == height) then winnerPrinter (moves-1) height
+    else do
+        c <- getLine
+        let com = words c
+        if(head com /= "q") then
+            if(head com == "b") then do
+                clr
+                -- reset all the tower-lists when starting a new game
+                let tower1 = [] :: [Int]
+                    tower2 = [] :: [Int]
+                    tower3 = [] :: [Int]
+                let moves = 0 
+                let height = read (head (tail com)) :: Int
+                if(height <= 1 || height > 12) then do
+                    print "Unvalid nr of rings. The nr of rings needs to be between 2 and 12."
+                    start 0 1 tower1 tower2 tower3
+                else do
                     goto(0,0)
                     drawTowers (height + 1) height
                     putStr "\ESC[0J"
                     drawRings (ringPos tower1 height) tower2 tower3 height height
                     goto(0, height + 2)
                     start 1 height (ringPos tower1 height) tower2 tower3
-              else do
+            else do
                 let f = read (head com) :: Int
                     t = read (last com) :: Int
                 if ((f>0 && f<4) && (t>0 && t<4)) then do
@@ -28,11 +33,23 @@ start moves height tower1 tower2 tower3 = do
                         let doneMove = doMove currentMove
                         statusPrinter moves height "             "
                         nextRound moves height f t doneMove tower1 tower2 tower3
-                    else do statusPrinter moves height "Unvalid move"
+                    else do statusPrinter (moves-1) height "Unvalid move"
                             start moves height tower1 tower2 tower3                        
-                else do statusPrinter moves height "Unvalid move"
+                else do statusPrinter (moves-1) height "Unvalid move"
                         start moves height tower1 tower2 tower3      
-          else return ()
+        else return ()
+
+statusPrinter moves height str = do goto(0, height + 2)
+                                    putStrLn ("Number of moves: " ++ show moves)
+                                    goto(30, height + 2)
+                                    putStrLn str
+                                    putStr "\ESC[0J"
+
+winnerPrinter moves height = do goto(0, height + 2)
+                                putStrLn ("You successfully completed the game on level " ++ show height ++ " using " ++ show moves ++ " moves!")
+                                main 
+
+ringPos towerNr n = reverse [x |x <- [1..n]]    
 
 legalMove ([], towerTo) = False
 legalMove (towerFrom, []) = True
@@ -51,68 +68,64 @@ findCorrectTower f t tower1 tower2 tower3 | (f==1 && t==2) = (tower1, tower2)
 nextRound moves height f t (towerFrom, towerTo) tower1 tower2 tower3 | (f==1 && t==2) = do goto(0,0)
                                                                                            drawTowers (height + 1) height
                                                                                            drawRings towerFrom towerTo tower3 height height
+                                                                                           goto(0,height+3)
                                                                                            start (moves+1) height towerFrom towerTo tower3               
                                                                      | (f==1 && t==3) = do goto(0,0)
                                                                                            drawTowers (height + 1) height
                                                                                            drawRings towerFrom tower2 towerTo height height
+                                                                                           goto(0,height+3)
                                                                                            start (moves+1) height towerFrom tower2 towerTo
                                                                      | (f==2 && t==3) = do goto(0,0)
                                                                                            drawTowers (height + 1) height
                                                                                            drawRings tower1 towerFrom towerTo height height
+                                                                                           goto(0,height+3)
                                                                                            start (moves+1) height tower1 towerFrom towerTo
                                                                      | (f==2 && t==1) = do goto(0,0)
                                                                                            drawTowers (height + 1) height
                                                                                            drawRings towerTo towerFrom tower3 height height
+                                                                                           goto(0,height+3)
                                                                                            start (moves+1) height towerTo towerFrom tower3
                                                                      | (f==3 && t==1) = do goto(0,0)
                                                                                            drawTowers (height + 1) height
                                                                                            drawRings towerTo tower2 towerFrom height height
+                                                                                           goto(0,height+3)
                                                                                            start (moves+1) height towerTo tower2 towerFrom
                                                                      | otherwise      = do goto(0,0)
                                                                                            drawTowers (height + 1) height
                                                                                            drawRings tower1 towerTo towerFrom height height
-                                                                                           start (moves+1) height tower1 towerTo towerFrom                          
+                                                                                           goto(0,height+3)
+                                                                                           start (moves+1) height tower1 towerTo towerFrom                                                 
 
-statusPrinter moves height str = do goto(0, height + 2)
-                                    putStrLn ("Number of moves: " ++ show moves)
-                                    goto(30, height + 2)
-                                    putStrLn str
-                                    putStr "\ESC[0J"
+drawRings tower1 tower2 tower3 height width = drawRingLayer tower1 tower2 tower3 height width
 
-ringPos towerNr n = reverse [x |x <- [1..n]]                           
-
-drawRings tower1 tower2 tower3 height width = drawRingsOdd tower1 tower2 tower3 height width
-
--- remove spacing
-
-drawRingsOdd [] [] [] height width = return ()
-drawRingsOdd [] [] tower3 height width = do writeAt ((5*width- ((head tower3) - 1)), height + 1) (concat (take (head tower3) (repeat("# "))))
-                                            drawRings [] [] (tail tower3) (height-1) width
-drawRingsOdd [] tower2 [] height width = do writeAt ((3*width - ((head tower2) - 1)), (height + 1)) (concat (take (head tower2) (repeat("# "))))
-                                            drawRings [] (tail tower2) [] (height-1) width
-drawRingsOdd tower1 [] [] height width = do writeAt ((width - (head tower1) + 1), (height + 1)) (concat (take (head tower1) (repeat("# "))))
-                                            drawRings (tail tower1) [] [] (height-1) width
-drawRingsOdd tower1 [] tower3 height width = do writeAt ((width - (head tower1) + 1), (height + 1)) (concat (take (head tower1) (repeat("# "))))
-                                                writeAt ((5*width- ((head tower3) - 1)), height + 1) (concat (take (head tower3) (repeat("# "))))
-                                                drawRings (tail tower1) [] (tail tower3) (height-1) width
-drawRingsOdd [] tower2 tower3 height width = do writeAt ((3*width - ((head tower2) - 1)), (height + 1)) (concat (take (head tower2) (repeat("# "))))                                        
-                                                writeAt ((5*width- ((head tower3) - 1)), height + 1) (concat (take (head tower3) (repeat("# "))))
-                                                drawRings [] (tail tower2) (tail tower3) (height-1) width
-drawRingsOdd tower1 tower2 [] height width = do writeAt ((width - (head tower1) + 1), (height + 1)) (concat (take (head tower1) (repeat("# "))))
-                                                writeAt ((3*width - ((head tower2) - 1)), (height + 1)) (concat (take (head tower2) (repeat("# "))))
-                                                drawRings (tail tower1) (tail tower2) [] (height-1) width
-drawRingsOdd tower1 tower2 tower3 height width = do writeAt ((height - (head tower1) + 1), (height + 1)) (concat (take (head tower1) (repeat("# "))))
-                                                    writeAt ((3*width - ((head tower2) - 1)), (height + 1)) (concat (take (head tower2) (repeat("# "))))                                        
-                                                    writeAt ((5*width- ((head tower3) - 1)), height + 1) (concat (take (head tower3) (repeat("# "))))
-                                                    drawRings (tail tower1) (tail tower2) (tail tower3) (height-1) width
+drawRingLayer [] [] [] height width = return ()
+drawRingLayer [] [] tower3 height width = do writeAt ((5*width- ((head tower3) - 1)), height + 1) (concat (take (head tower3) (repeat("# "))))
+                                             drawRings [] [] (tail tower3) (height-1) width
+drawRingLayer [] tower2 [] height width = do writeAt ((3*width - ((head tower2) - 1)), (height + 1)) (concat (take (head tower2) (repeat("# "))))
+                                             drawRings [] (tail tower2) [] (height-1) width
+drawRingLayer tower1 [] [] height width = do writeAt ((width - (head tower1) + 1), (height + 1)) (concat (take (head tower1) (repeat("# "))))
+                                             drawRings (tail tower1) [] [] (height-1) width
+drawRingLayer tower1 [] tower3 height width = do writeAt ((width - (head tower1) + 1), (height + 1)) (concat (take (head tower1) (repeat("# "))))
+                                                 writeAt ((5*width- ((head tower3) - 1)), height + 1) (concat (take (head tower3) (repeat("# "))))
+                                                 drawRings (tail tower1) [] (tail tower3) (height-1) width
+drawRingLayer [] tower2 tower3 height width = do writeAt ((3*width - ((head tower2) - 1)), (height + 1)) (concat (take (head tower2) (repeat("# "))))                                        
+                                                 writeAt ((5*width- ((head tower3) - 1)), height + 1) (concat (take (head tower3) (repeat("# "))))
+                                                 drawRings [] (tail tower2) (tail tower3) (height-1) width
+drawRingLayer tower1 tower2 [] height width = do writeAt ((width - (head tower1) + 1), (height + 1)) (concat (take (head tower1) (repeat("# "))))
+                                                 writeAt ((3*width - ((head tower2) - 1)), (height + 1)) (concat (take (head tower2) (repeat("# "))))
+                                                 drawRings (tail tower1) (tail tower2) [] (height-1) width
+drawRingLayer tower1 tower2 tower3 height width = do writeAt ((height - (head tower1) + 1), (height + 1)) (concat (take (head tower1) (repeat("# "))))
+                                                     writeAt ((3*width - ((head tower2) - 1)), (height + 1)) (concat (take (head tower2) (repeat("# "))))                                        
+                                                     writeAt ((5*width- ((head tower3) - 1)), height + 1) (concat (take (head tower3) (repeat("# "))))
+                                                     drawRings (tail tower1) (tail tower2) (tail tower3) (height-1) width
                             
 -- draws all the tower-layers
 drawTowers height width = if height == 0 then return ()
-                           else do writeRow height width
-                                   drawTowers (height - 1) width
+                          else do writeTowerLayer height width
+                                  drawTowers (height - 1) width
 
 -- draws each single tower-layer
-writeRow height width = putStrLn (concat (take 3 (repeat ((concat (take (width-1) (repeat " "))) ++ "|" ++ (concat (take width (repeat " ")))))))
+writeTowerLayer height width = putStrLn (concat (take 3 (repeat ((concat (take (width-1) (repeat " "))) ++ "|" ++ (concat (take width (repeat " ")))))))
 
 writeAt (x,y) str = do goto (x,y)
                        putStr str
@@ -126,4 +139,4 @@ goto(x,y) = putStr("\ESC["++ show y ++";" ++ show x++"H")
 
 main :: IO ()
 main = do putStrLn "Start a new game with: b <nbOfRings>, or quit with: q "
-          start 0 1 ([] :: [Int]) ([] :: [Int]) ([] :: [Int]) 
+          start 0 1 [] [] [] 
