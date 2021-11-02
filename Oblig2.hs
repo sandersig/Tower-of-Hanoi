7a52a3219d252b765881e3fbc5867c43fe38ad16
Sander Sigmundstad <sanderls@icloud.com>
2021-11-02 09:48:20 +0100

    Added type to all methods

--- Oblig2.hs
+++ Oblig2.hs
@@ -2,20 +2,19 @@ import Text.Read
 import Data.Maybe
 
 start :: IO ()
-start = do
-    c <- getLine
-    let com = words c
-    if(com == []) then start
-    else if(head com == "q") then return ()
-    else if(head com == "b") then do 
-        let h = readMaybe (head (tail com)) :: Maybe Int
-        if(isNothing h) then
-            start
-        else startGame (fromJust h)
-    else do putStrLn "Unknown command" 
-            start
-    
-hanoi moves history height tower1 tower2 tower3 str = do
+start = do  c <- getLine
+            let com = words c
+            if(com == []) then start
+            else if(head com == "q") then return ()
+            else if(head com == "b") then do 
+                let h = readMaybe (head (tail com)) :: Maybe Int
+                if(isNothing h) then start
+                else startGame (fromJust h)
+            else do putStrLn "Unknown command" 
+                    start
+
+hanoi :: Int -> [([Int], [Int], [Int])] -> Int -> [Int] -> [Int] -> [Int] -> String -> IO () 
+hanoi moves history height tower1 tower2 tower3 str =
     if(length tower3 == height) then winnerPrinter (moves-1) height
     else do
         statusPrinter moves height str
@@ -25,24 +24,19 @@ hanoi moves history height tower1 tower2 tower3 str = do
         else if(head com /= "q") then
             if(head com == "b") then do
                 let h = readMaybe (head (tail com)) :: Maybe Int
-                if(isNothing h) then
-                    hanoi moves history height tower1 tower2 tower3 str
+                if(isNothing h) then hanoi moves history height tower1 tower2 tower3 str
                 else startGame (fromJust h) 
             else if(head com == "z") then do
                 let x = readMaybe (head (tail com)) :: Maybe Int
-                if(isNothing x) then
-                    hanoi moves history height tower1 tower2 tower3 str
+                if(isNothing x) then hanoi moves history height tower1 tower2 tower3 str
                 else do
                     let n = fromJust x    
-                    if((abs n) >= moves) then
-                        resetAllMoves height history str
-                    else do
-                        undoNMoves height history moves (abs n) str   
+                    if((abs n) >= moves) then resetAllMoves height history str
+                    else undoNMoves height history moves (abs n) str   
             else do
                 let x = readMaybe (head com) :: Maybe Int
                     y = readMaybe (last com) :: Maybe Int
-                if(isNothing x || isNothing y) then 
-                    hanoi moves history height tower1 tower2 tower3 "Unvalid move"
+                if(isNothing x || isNothing y) then hanoi moves history height tower1 tower2 tower3 "Unvalid move"
                 else do
                 let f = fromJust x
                     t = fromJust y
@@ -55,6 +49,7 @@ hanoi moves history height tower1 tower2 tower3 str = do
                 else hanoi moves history height tower1 tower2 tower3 "Unvalid move"     
         else return ()
 
+startGame :: Int -> IO ()
 startGame height = do  let moves = 0
                            tower1 = [] :: [Int]
                            tower2 = [] :: [Int]
@@ -75,6 +70,7 @@ drawBoard height = do clr
                       drawTowers (height + 1) height
                       putStr "\ESC[0J"
 
+undoNMoves :: Int -> [([Int], [Int], [Int])] -> Int -> Int -> String -> IO ()
 undoNMoves height history moves n str = do goto(0,0) 
                                            drawTowers (height + 1) height
                                            let newHistory = (last (take (length history - n) history))
@@ -82,6 +78,7 @@ undoNMoves height history moves n str = do goto(0,0)
                                            hanoi (moves-n) [newHistory] height (fst3 newHistory) (snd3 newHistory) (trd3 newHistory) str
 
 --lagrer ikke korrekt, resetter til 2. siste history
+resetAllMoves :: Int -> [([Int], [Int], [Int])] -> String -> IO ()
 resetAllMoves height history str = do drawBoard height
                                       drawRings (ringPos (head history) height) [] [] height height
                                       let moves = 0
@@ -94,27 +91,34 @@ snd3 (_, x, _) = x
 trd3 :: ([Int], [Int], [Int]) -> [Int]
 trd3 (_, _, x) = x
 
+statusPrinter :: Show a => a -> Int -> String -> IO ()
 statusPrinter moves height str = do goto(0, height + 2)
                                     putStrLn ("Number of moves: " ++ show moves)
                                     goto(30, height + 2)
                                     putStrLn str
                                     putStr "\ESC[0J"
 
+winnerPrinter :: Show a => a -> Int -> IO ()
 winnerPrinter moves height = do goto(0, height + 2)
                                 putStrLn ("You successfully completed the game on level " ++ show height ++ " using " ++ show moves ++ " moves!")
                                 main 
 
+addHistory :: [([Int], [Int], [Int])] -> [Int] -> [Int] -> [Int] -> [([Int], [Int], [Int])]
 addHistory history tower1 tower2 tower3 = history ++ [(tower1, tower2, tower3)]
 
+ringPos :: (Num a, Enum a) => p -> a -> [a]
 ringPos towerNr n = reverse [x |x <- [1..n]]    
 
+legalMove :: ([Int], [Int]) -> Bool
 legalMove ([], towerTo)        = False
 legalMove (towerFrom, [])      = True
 legalMove (towerFrom, towerTo) | (last towerFrom > last towerTo) = False
                                | otherwise                       = True
-                         
+
+doMove :: ([Int], [Int]) -> ([Int], [Int])
 doMove (towerFrom, towerTo) = ((init towerFrom), (towerTo ++ [last towerFrom]))
 
+findCorrectTower :: (Eq a1, Eq a2, Num a1, Num a2) => a1 -> a2 -> [Int] -> [Int] -> [Int] -> ([Int], [Int])
 findCorrectTower f t tower1 tower2 tower3 | (f==1 && t==2) = (tower1, tower2)
                                           | (f==1 && t==3) = (tower1, tower3)
                                           | (f==2 && t==3) = (tower2, tower3)
@@ -122,23 +126,27 @@ findCorrectTower f t tower1 tower2 tower3 | (f==1 && t==2) = (tower1, tower2)
                                           | (f==3 && t==1) = (tower3, tower1)
                                           | otherwise      = (tower3, tower2)
 
-
+nextRoundHelper :: Int -> [([Int], [Int], [Int])] -> Int -> String -> [Int] -> [Int] -> [Int] -> IO ()
 nextRoundHelper moves history height str tower1 tower2 tower3 = do  goto(0,0)
                                                                     drawTowers (height + 1) height
                                                                     drawRings tower1 tower2 tower3 height height
                                                                     goto(0,height+3)
                                                                     hanoi (moves+1) (addHistory history tower1 tower2 tower3) height tower1 tower2 tower3 str
 
+nextRound :: (Eq a1, Eq a2, Num a1, Num a2) => Int -> [([Int], [Int], [Int])] -> Int -> a1 -> a2 -> ([Int], [Int]) -> [Int] -> [Int] -> [Int] -> String -> IO ()
 nextRound moves history height f t (towerFrom, towerTo) tower1 tower2 tower3 str    | (f==1 && t==2) = nextRoundHelper moves history height str towerFrom towerTo tower3
                                                                                     | (f==1 && t==3) = nextRoundHelper moves history height str towerFrom tower2 towerTo
                                                                                     | (f==2 && t==3) = nextRoundHelper moves history height str tower1 towerFrom towerTo
                                                                                     | (f==2 && t==1) = nextRoundHelper moves history height str towerTo towerFrom tower3
                                                                                     | (f==3 && t==1) = nextRoundHelper moves history height str towerTo tower2 towerFrom
                                                                                     | otherwise      = nextRoundHelper moves history height str tower1 towerTo towerFrom                                               
--- refaktorer?
+
+drawRings :: [Int] -> [Int] -> [Int] -> Int -> Int -> IO ()
 drawRings tower1 tower2 tower3 height width = do drawRingLayer tower1 tower2 tower3 height width
                                                  goto(0, height + 2)
-                                                 
+
+-- refaktorer?
+drawRingLayer :: [Int] -> [Int] -> [Int] -> Int -> Int -> IO ()
 drawRingLayer [] [] [] height width             = return ()
 drawRingLayer [] [] tower3 height width         = do drawRingLayerHelper 5 width height tower3
                                                      drawRings [] [] (tail tower3) (height-1) width
@@ -160,20 +168,24 @@ drawRingLayer tower1 tower2 tower3 height width = do drawRingLayerHelper 1 width
                                                      drawRingLayerHelper 5 width height tower3
                                                      drawRings (tail tower1) (tail tower2) (tail tower3) (height-1) width
 
+drawRingLayerHelper :: Int -> Int -> Int -> [Int] -> IO ()
 drawRingLayerHelper n width height towerNr = writeAt ((n*width- ((head towerNr) - 1)), height + 1) (concat (take (head towerNr) (repeat("# "))))
                             
 -- draws all the tower-layers
+drawTowers :: (Eq t, Num t) => t -> Int -> IO ()
 drawTowers 0 _ = return ()
 drawTowers height width = do writeTowerLayer height width
                              drawTowers (height - 1) width
 
 -- draws each single tower-layer
+writeTowerLayer :: p -> Int -> IO ()
 writeTowerLayer height width = putStrLn (concat (take 3 (repeat ((concat (take (width-1) (repeat " "))) ++ "|" ++ (concat (take width (repeat " ")))))))
 
 writeAt (x,y) str = do goto (x,y)
                        putStr str
 
 --clears the terminal-window
+clr :: IO ()
 clr = putStr "\ESC[2J"
 
 -- changes pos of marker
